47be699f2c771f5645daed0c39694d590a4e5ee0
Cameron Browning <camb416@gmail.com>
2015-09-08 16:18:27 -0400

    partial import commenting

--- exports/2014/importall.sh
+++ exports/2014/importall.sh
@@ -2,49 +2,49 @@
 # example: wp fmagimport test 4.inc
 # 
 # 
-wp fmagimport test 4626.inc
-wp fmagimport test 4628.inc
-wp fmagimport test 4631.inc
-wp fmagimport test 4633.inc
-wp fmagimport test 4636.inc
-wp fmagimport test 4638.inc
-wp fmagimport test 4642.inc
-wp fmagimport test 4645.inc
-wp fmagimport test 4647.inc
-wp fmagimport test 4649.inc
-wp fmagimport test 4651.inc
-wp fmagimport test 4653.inc
-wp fmagimport test 4655.inc
-wp fmagimport test 4656.inc
-wp fmagimport test 4658.inc
-wp fmagimport test 4665.inc
-wp fmagimport test 4667.inc
-wp fmagimport test 4669.inc
-wp fmagimport test 4671.inc
-wp fmagimport test 4673.inc
-wp fmagimport test 4675.inc
-wp fmagimport test 4677.inc
-wp fmagimport test 4679.inc
-wp fmagimport test 4681.inc
-wp fmagimport test 4683.inc
-wp fmagimport test 4685.inc
-wp fmagimport test 4689.inc
-wp fmagimport test 4691.inc
-wp fmagimport test 4693.inc
-wp fmagimport test 4695.inc
-wp fmagimport test 4697.inc
-wp fmagimport test 4699.inc
-wp fmagimport test 4701.inc
-wp fmagimport test 4703.inc
-wp fmagimport test 4705.inc
-wp fmagimport test 4707.inc
-wp fmagimport test 4719.inc
-wp fmagimport test 4757.inc
-wp fmagimport test 4722.inc
-wp fmagimport test 4724.inc
-wp fmagimport test 4728.inc
-wp fmagimport test 4734.inc
-wp fmagimport test 4736.inc
+# wp fmagimport test 4626.inc
+# wp fmagimport test 4628.inc
+# wp fmagimport test 4631.inc
+# wp fmagimport test 4633.inc
+# wp fmagimport test 4636.inc
+# wp fmagimport test 4638.inc
+# wp fmagimport test 4642.inc
+# wp fmagimport test 4645.inc
+# wp fmagimport test 4647.inc
+# wp fmagimport test 4649.inc
+# wp fmagimport test 4651.inc
+# wp fmagimport test 4653.inc
+# wp fmagimport test 4655.inc
+# wp fmagimport test 4656.inc
+# wp fmagimport test 4658.inc
+# wp fmagimport test 4665.inc
+# wp fmagimport test 4667.inc
+# wp fmagimport test 4669.inc
+# wp fmagimport test 4671.inc
+# wp fmagimport test 4673.inc
+# wp fmagimport test 4675.inc
+# wp fmagimport test 4677.inc
+# wp fmagimport test 4679.inc
+# wp fmagimport test 4681.inc
+# wp fmagimport test 4683.inc
+# wp fmagimport test 4685.inc
+# wp fmagimport test 4689.inc
+# wp fmagimport test 4691.inc
+# wp fmagimport test 4693.inc
+# wp fmagimport test 4695.inc
+# wp fmagimport test 4697.inc
+# wp fmagimport test 4699.inc
+# wp fmagimport test 4701.inc
+# wp fmagimport test 4703.inc
+# wp fmagimport test 4705.inc
+# wp fmagimport test 4707.inc
+# wp fmagimport test 4719.inc
+# wp fmagimport test 4757.inc
+# wp fmagimport test 4722.inc
+# wp fmagimport test 4724.inc
+# wp fmagimport test 4728.inc
+# wp fmagimport test 4734.inc
+# wp fmagimport test 4736.inc
 wp fmagimport test 4738.inc
 wp fmagimport test 4746.inc
 wp fmagimport test 4750.inc
