4bfd347e02e9ec3d7ff3b9e79f97d1a071d64ce4
Cameron Browning <camb416@gmail.com>
2015-09-16 01:40:39 -0400

    added cover exports from jan 2015

---
+++ exports/2015/01/exportCovers.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+drush ne-export 5001 > 5001.inc
+drush ne-export 5002 > 5002.inc
+drush ne-export 5004 > 5004.inc
+drush ne-export 5006 > 5006.inc
+drush ne-export 5007 > 5007.inc
+drush ne-export 5008 > 5008.inc
+drush ne-export 5010 > 5010.inc
+drush ne-export 5011 > 5011.inc
+drush ne-export 5012 > 5012.inc
---
+++ exports/2015/01/importCovers.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+wp fmagimport cover  5001.inc
+wp fmagimport cover  5002.inc
+wp fmagimport cover  5004.inc
+wp fmagimport cover  5006.inc
+wp fmagimport cover  5007.inc
+wp fmagimport cover  5008.inc
+wp fmagimport cover  5010.inc
+wp fmagimport cover  5011.inc
+wp fmagimport cover  5012.inc
