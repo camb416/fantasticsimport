7c2ced67af7ddc5616d0a5a0af410a8db7c03c98
Cameron Browning <camb416@gmail.com>
2015-10-04 22:30:06 -0400

    fixed typo in updater

--- exports/2015/01/updateAll.sh
+++ exports/2015/01/updateAll.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
- up fmagimport node update 5000.inc
- up fmagimport node update 5001.inc
- up fmagimport node update 5002.inc
- up fmagimport node update 5003.inc
- up fmagimport node update 5004.inc
- up fmagimport node update 5005.inc
- up fmagimport node update 5006.inc
- up fmagimport node update 5007.inc
- up fmagimport node update 5008.inc
- up fmagimport node update 5009.inc
- up fmagimport node update 5010.inc
- up fmagimport node update 5011.inc
- up fmagimport node update 5012.inc
+wp fmagimport node update 5000.inc
+wp fmagimport node update 5001.inc
+wp fmagimport node update 5002.inc
+wp fmagimport node update 5003.inc
+wp fmagimport node update 5004.inc
+wp fmagimport node update 5005.inc
+wp fmagimport node update 5006.inc
+wp fmagimport node update 5007.inc
+wp fmagimport node update 5008.inc
+wp fmagimport node update 5009.inc
+wp fmagimport node update 5010.inc
+wp fmagimport node update 5011.inc
+wp fmagimport node update 5012.inc
