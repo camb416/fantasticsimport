746be485eec0a524b257bcbd40b1b7518f837e34
Cameron Browning <camb416@gmail.com>
2016-03-10 13:59:39 -0500

    added export script for jan feb 2016:

---
+++ exports/2016/01-02/exportAll.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+# example: drush ne-export 4 > 4.inc
+
+# The Query used to generate the list
+# SELECT nid
+# FROM node
+# WHERE FROM_UNIXTIME(created,"%m,%Y") = "01,2016"
+# OR
+# FROM_UNIXTIME(created,"%m,%Y") = "02,2016";
+
+drush ne-export 5359 > 5359.inc
+drush ne-export 5360 > 5360.inc
+drush ne-export 5361 > 5361.inc
+drush ne-export 5362 > 5362.inc
+drush ne-export 5363 > 5363.inc
+drush ne-export 5366 > 5366.inc
+drush ne-export 5369 > 5369.inc
+drush ne-export 5370 > 5370.inc
+drush ne-export 5371 > 5371.inc
+drush ne-export 5372 > 5372.inc
+drush ne-export 5373 > 5373.inc
+drush ne-export 5374 > 5374.inc
+drush ne-export 5375 > 5375.inc
+drush ne-export 5376 > 5376.inc
+drush ne-export 5377 > 5377.inc
+drush ne-export 5378 > 5378.inc
+drush ne-export 5379 > 5379.inc
+drush ne-export 5380 > 5380.inc
+drush ne-export 5381 > 5381.inc
+drush ne-export 5382 > 5382.inc
+drush ne-export 5383 > 5383.inc
+drush ne-export 5384 > 5384.inc
+drush ne-export 5385 > 5385.inc
+drush ne-export 5386 > 5386.inc
+drush ne-export 5387 > 5387.inc
+drush ne-export 5390 > 5390.inc
+drush ne-export 5391 > 5391.inc
+drush ne-export 5392 > 5392.inc
+drush ne-export 5393 > 5393.inc
+drush ne-export 5394 > 5394.inc
+drush ne-export 5395 > 5395.inc
+drush ne-export 5396 > 5396.inc
+drush ne-export 5397 > 5397.inc
+drush ne-export 5398 > 5398.inc
+drush ne-export 5399 > 5399.inc
---
+++ exports/2016/01-02/importAll.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# this imports all the covers and stories from 2016 Jan Feb
+
+# example
+# wp fmagimport node create 5316.inc
