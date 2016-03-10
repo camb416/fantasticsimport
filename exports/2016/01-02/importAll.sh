ad53fe5348d207b25ef0432b701639fbb9319521
Cameron Browning <camb416@gmail.com>
2016-03-10 14:00:46 -0500

    updated import script

--- exports/2016/01-02/importAll.sh
+++ exports/2016/01-02/importAll.sh
@@ -3,3 +3,38 @@
 
 # example
 # wp fmagimport node create 5316.inc
+wp fmagimport node create 5359.inc
+wp fmagimport node create 5360.inc
+wp fmagimport node create 5361.inc
+wp fmagimport node create 5362.inc
+wp fmagimport node create 5363.inc
+wp fmagimport node create 5366.inc
+wp fmagimport node create 5369.inc
+wp fmagimport node create 5370.inc
+wp fmagimport node create 5371.inc
+wp fmagimport node create 5372.inc
+wp fmagimport node create 5373.inc
+wp fmagimport node create 5374.inc
+wp fmagimport node create 5375.inc
+wp fmagimport node create 5376.inc
+wp fmagimport node create 5377.inc
+wp fmagimport node create 5378.inc
+wp fmagimport node create 5379.inc
+wp fmagimport node create 5380.inc
+wp fmagimport node create 5381.inc
+wp fmagimport node create 5382.inc
+wp fmagimport node create 5383.inc
+wp fmagimport node create 5384.inc
+wp fmagimport node create 5385.inc
+wp fmagimport node create 5386.inc
+wp fmagimport node create 5387.inc
+wp fmagimport node create 5390.inc
+wp fmagimport node create 5391.inc
+wp fmagimport node create 5392.inc
+wp fmagimport node create 5393.inc
+wp fmagimport node create 5394.inc
+wp fmagimport node create 5395.inc
+wp fmagimport node create 5396.inc
+wp fmagimport node create 5397.inc
+wp fmagimport node create 5398.inc
+wp fmagimport node create 5399.inc
