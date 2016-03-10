0ae4e1bd5eea14318ff97a1cb03f5b229fbe0990
Cameron Browning <camb416@gmail.com>
2016-03-10 13:30:04 -0500

    updated scripts for 2015

---
+++ exports/2015/11-12/exportAll.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+# example: drush ne-export 4 > 4.inc
+
+# The Query used to generate the list
+# SELECT nid
+# FROM node
+# WHERE FROM_UNIXTIME(created,"%m,%Y") = "11,2015"
+# OR
+# FROM_UNIXTIME(created,"%m,%Y") = "12,2015";
+
+
+drush ne-export 5316 > 5316.inc
+drush ne-export 5317 > 5317.inc
+drush ne-export 5318 > 5318.inc
+drush ne-export 5319 > 5319.inc
+drush ne-export 5320 > 5320.inc
+drush ne-export 5321 > 5321.inc
+drush ne-export 5322 > 5322.inc
+drush ne-export 5323 > 5323.inc
+drush ne-export 5324 > 5324.inc
+drush ne-export 5325 > 5325.inc
+drush ne-export 5326 > 5326.inc
+drush ne-export 5328 > 5328.inc
+drush ne-export 5329 > 5329.inc
+drush ne-export 5330 > 5330.inc
+drush ne-export 5331 > 5331.inc
+drush ne-export 5332 > 5332.inc
+drush ne-export 5333 > 5333.inc
+drush ne-export 5334 > 5334.inc
+drush ne-export 5335 > 5335.inc
+drush ne-export 5336 > 5336.inc
+drush ne-export 5337 > 5337.inc
+drush ne-export 5338 > 5338.inc
+drush ne-export 5339 > 5339.inc
+drush ne-export 5340 > 5340.inc
+drush ne-export 5341 > 5341.inc
+drush ne-export 5342 > 5342.inc
+drush ne-export 5343 > 5343.inc
+drush ne-export 5344 > 5344.inc
+drush ne-export 5345 > 5345.inc
+drush ne-export 5346 > 5346.inc
+drush ne-export 5347 > 5347.inc
+drush ne-export 5348 > 5348.inc
+drush ne-export 5349 > 5349.inc
+drush ne-export 5350 > 5350.inc
+drush ne-export 5351 > 5351.inc
+drush ne-export 5352 > 5352.inc
+drush ne-export 5353 > 5353.inc
+drush ne-export 5354 > 5354.inc
+drush ne-export 5355 > 5355.inc
+drush ne-export 5356 > 5356.inc
+drush ne-export 5357 > 5357.inc
+drush ne-export 5358 > 5358.inc
+drush ne-export 5364 > 5364.inc
+drush ne-export 5365 > 5365.inc
+drush ne-export 5367 > 5367.inc
+drush ne-export 5368 > 5368.inc
+
+
+
