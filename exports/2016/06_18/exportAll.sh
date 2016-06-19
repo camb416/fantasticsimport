5b3ed11c92ee8f8d0ff7f9344fea9560ed3dae28
Cameron Browning <browning@wearesubrosa.com>
2016-06-19 00:29:52 -0400

    added exports up to the 18th

---
+++ exports/2016/06_18/exportAll.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+drush ne-export 5432 > 5432.inc
+drush ne-export 5439 > 5439.inc
+drush ne-export 5440 > 5440.inc
+drush ne-export 5441 > 5441.inc
+drush ne-export 5442 > 5442.inc
+drush ne-export 5443 > 5443.inc
+drush ne-export 5444 > 5444.inc
+drush ne-export 5445 > 5445.inc
+drush ne-export 5446 > 5446.inc
---
+++ exports/2016/06_18/importAll.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+wp fmagimport node create 5432.inc
+wp fmagimport node create 5439.inc
+wp fmagimport node create 5440.inc
+wp fmagimport node create 5441.inc
+wp fmagimport node create 5442.inc
+wp fmagimport node create 5443.inc
+wp fmagimport node create 5444.inc
+wp fmagimport node create 5445.inc
+wp fmagimport node create 5446.inc
