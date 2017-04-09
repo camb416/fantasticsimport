8d2a7846e7a365ae22fd7121cbfbc7511b5c4ec6
Cameron Browning <camb416@gmail.com>
2017-04-09 01:33:15 -0400

    added content up to april 9th

---
+++ exports/2017/04/exportall.sh
@@ -0,0 +1,11 @@
+#/bin/bash
+
+drush ne-export 5618 > 5618.inc
+drush ne-export 5619 > 5619.inc
+drush ne-export 5620 > 5620.inc
+drush ne-export 5621 > 5621.inc
+drush ne-export 5622 > 5622.inc
+drush ne-export 5623 > 5623.inc
+drush ne-export 5624 > 5624.inc
+drush ne-export 5625 > 5625.inc
+drush ne-export 5626 > 5626.inc
---
+++ exports/2017/04/importall.sh
@@ -0,0 +1,11 @@
+#/bin/bash
+
+wp fmagimport node create 5618.inc
+wp fmagimport node create 5619.inc
+wp fmagimport node create 5620.inc
+wp fmagimport node create 5621.inc
+wp fmagimport node create 5622.inc
+wp fmagimport node create 5623.inc
+wp fmagimport node create 5624.inc
+wp fmagimport node create 5625.inc
+wp fmagimport node create 5626.inc
