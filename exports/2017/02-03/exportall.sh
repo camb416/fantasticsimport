8629335cc84ee15af62379d38502312eccb12899
Cameron Browning <camb416@gmail.com>
2017-04-09 01:28:25 -0400

    added feb and march nodes

---
+++ exports/2017/02-03/exportall.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# february
+drush ne-export 5596 > 5596.inc
+drush ne-export 5597 > 5597.inc
+drush ne-export 5598 > 5598.inc
+drush ne-export 5599 > 5599.inc
+drush ne-export 5600 > 5600.inc
+drush ne-export 5601 > 5601.inc
+
+# march
+drush ne-export 5602 > 5602.inc
+drush ne-export 5603 > 5603.inc
+drush ne-export 5604 > 5604.inc
+drush ne-export 5605 > 5605.inc
+drush ne-export 5606 > 5606.inc
+drush ne-export 5607 > 5607.inc
+drush ne-export 5608 > 5608.inc
+drush ne-export 5609 > 5609.inc
+drush ne-export 5610 > 5610.inc
+drush ne-export 5611 > 5611.inc
+drush ne-export 5612 > 5612.inc
+drush ne-export 5614 > 5614.inc
+drush ne-export 5615 > 5615.inc
+drush ne-export 5616 > 5616.inc
+drush ne-export 5617 > 5617.inc
---
+++ exports/2017/02-03/importall.sh
@@ -0,0 +1,24 @@
+#feb
+wp fmagimport node create 5596.inc
+wp fmagimport node create 5597.inc
+wp fmagimport node create 5598.inc
+wp fmagimport node create 5599.inc
+wp fmagimport node create 5600.inc
+wp fmagimport node create 5601.inc
+
+#mar
+wp fmagimport node create 5602.inc
+wp fmagimport node create 5603.inc
+wp fmagimport node create 5604.inc
+wp fmagimport node create 5605.inc
+wp fmagimport node create 5606.inc
+wp fmagimport node create 5607.inc
+wp fmagimport node create 5608.inc
+wp fmagimport node create 5609.inc
+wp fmagimport node create 5610.inc
+wp fmagimport node create 5611.inc
+wp fmagimport node create 5612.inc
+wp fmagimport node create 5614.inc
+wp fmagimport node create 5615.inc
+wp fmagimport node create 5616.inc
+wp fmagimport node create 5617.inc
